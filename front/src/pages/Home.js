3484759f95d0a56845d99339894cf7ecc7395da9
dimad2602 <dimad26002@gmail.com>
2023-01-14 21:32:15 +0700

    Главная страница 3 картинки

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -3,9 +3,10 @@ import React from 'react';
 
 import homePNG1 from "../assets/homePNG1.png";
 import homePNG2 from "../assets/homePNG2.png";
+import homePNG3 from "../assets/homePNG3.png";
 
 
-const BackgroundPNGTest = styled.div`
+const BackgroundPNG1 = styled.div`
     background-image: url(${homePNG1}), url(${homePNG2});
     background-repeat: no-repeat, repeat;
     background-position: top-left center;
@@ -13,6 +14,21 @@ const BackgroundPNGTest = styled.div`
     ${'' /* Для моего экрана */}
     height: 93.0vh; 
     
+`
+
+const BackgroundPNG2 = styled.div`
+    background-image: url(${homePNG2});
+    background-repeat: no-repeat;
+    background-position: top-left;
+
+    height: 100vh; 
+`
+const BackgroundPNG3 = styled.div`
+    background-image: url(${homePNG3});
+    background-repeat: no-repeat;
+    background-position: center;
+    
+    height: 100vh; 
 `
 
 const Square = styled.div`
@@ -23,16 +39,34 @@ const Square = styled.div`
     left: 20%;
     top: 15vh;
 
-    
     background: #2D2D2D;
     ${'' /* mix-blend-mode: multiply; */}
     background: rgba( 0, 0, 0, 0.75);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);  
+`
+
+const Square2 = styled.div`
+    position: absolute;
+    width: 60%;
+    height: 80%;   
+    left: 20%;
+    top: 115%;
+
+    background: #2D2D2D;
+    background: rgba( 0, 0, 0, 0.75);
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`
 
-    p& {
+const Square3 = styled.div`
+    position: absolute;
+    width: 60%;
+    height: 80%;   
+    left: 20%;
+    top: 215%;
 
-    }
-    
+    background: #2D2D2D;
+    background: rgba( 0, 0, 0, 0.75);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `
 
 const Text = styled.div`
@@ -58,7 +92,7 @@ const Text2 = styled.div`
     ${'' /* font-family: 'Inter'; */}
     font-style: normal;
     
-    margin-top: 50px;
+    margin-top: 70px;
     margin-left: 50px;
     margin-right: 50px;
     align-items: center;
@@ -72,7 +106,7 @@ const Text2 = styled.div`
 const Home = () => {
     return (
             <>
-            <BackgroundPNGTest>
+            <BackgroundPNG1>
                 <Square>
                         <Text> 
                             Почему мы?
@@ -87,7 +121,35 @@ const Home = () => {
                             Обзоры, гайды, статьи и многое другое мы собрали специально для вас в одном месте
                         </Text2>
                 </Square> 
-            </BackgroundPNGTest>    
+            </BackgroundPNG1>
+            <BackgroundPNG2>
+                <Square2>
+                        <Text> 
+                            Почему мы?
+                        </Text>
+                        <Text2>
+                            Мы предлагаем удобный механизм, который должен находить наилучшую комбинацию из доступных в продаже 
+                            комплектующих ПК исходя из всесторонней оценки их 
+                            взаимной совместимости и финансовых ресурсов пользователя и его потребностей
+                        </Text2>
+                </Square2> 
+            </BackgroundPNG2>
+            <BackgroundPNG3>
+                <Square3>
+                        <Text> 
+                            Обратитесь к нам если
+                        </Text>
+                        <Text2>
+                            Вы хотите приобрести максимально производительный компьютер за свои деньги
+                        </Text2>
+                        <Text2>
+                            Вы планируете приобрести большое количество компьютеров для офиса или компании
+                        </Text2>
+                        <Text2>
+                            Вы испытываете трудности при сборке компьютера
+                        </Text2>
+                </Square3> 
+            </BackgroundPNG3>   
             </>
     );
 };
